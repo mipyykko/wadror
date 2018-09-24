a6982c5784dd45eee2c16ba6ee1b1e7e9aae0147
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2018-09-24 16:57:04 +0300

    heroku hell #2

--- db/migrate/20180923220458_add_user_id_to_ratings.rb
+++ db/migrate/20180923220458_add_user_id_to_ratings.rb
@@ -1,5 +1,5 @@
 class AddUserIdToRatings < ActiveRecord::Migration[5.2]
-  #def change
-  #  add_column :ratings, :user_id, :integer
-  #end
+  def change
+    add_column :ratings, :user_id, :integer
+  end
 end
