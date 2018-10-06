467ac575805b4a36d80e1835b30b8365be118d9f
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2018-10-06 16:03:20 +0300

    fixed some migrations

--- db/migrate/20181006110400_migrate_styles_from_beers.rb
+++ db/migrate/20181006110400_migrate_styles_from_beers.rb
@@ -1,6 +1,6 @@
 class MigrateStylesFromBeers < ActiveRecord::Migration[5.2]
   def change
     uniq_styles = Beer.all.map { |beer| beer.style }.to_set.to_a
-    uniq_styles.each { |style| Style.create name: style}
+    uniq_styles.map { |style| Style.create name: style}
   end
 end
