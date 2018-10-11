31689799ce6c970711faa16fa37ce7b1fc9c25a5
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2018-10-11 23:02:32 +0300

    debroke tests

--- spec/features/breweries_page_spec.rb
+++ spec/features/breweries_page_spec.rb
@@ -4,7 +4,7 @@ describe 'Breweries page' do
   it 'should not have anything before something created' do
     visit breweries_path
     expect(page).to have_content 'Breweries'
-    expect(page).to have_content 'Number of breweries: 0'
+    expect(page).to have_content 'Number of active breweries: 0'
   end
 
   describe 'when breweries exists' do
@@ -12,18 +12,25 @@ describe 'Breweries page' do
       y = 1999
       @breweries = ['asdf', 'bsdf', 'qwerty']
       @breweries.each do |b|
-        FactoryBot.create(:brewery, name: b, year: y += 1)
+        FactoryBot.create(:brewery, name: b, year: y += 1, active: true)
       end
+      FactoryBot.create(:brewery, name: "retired_brewery", year: 2000)
 
       visit breweries_path
     end
   
-    it 'lists the existing breweries and count' do
-      expect(page).to have_content "Number of breweries: 3"
+    it 'lists the existing active breweries and count' do
+      expect(page).to have_content "Number of active breweries: 3"
       
       @breweries.each do |b|
         expect(page).to have_content b
       end
+    end
+
+    it 'lists the existing retired breweries and count' do
+      expect(page).to have_content "Number of retired breweries: 1"
+      
+      expect(page).to have_content "retired_brewery"
     end
 
     it 'allows user to navigate to brewery page' do
